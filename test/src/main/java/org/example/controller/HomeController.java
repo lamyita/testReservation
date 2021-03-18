b3c2e23f33ea084bc230632b445da3f6b23f7dbe
lamyita <50775487+lamyita@users.noreply.github.com>
2021-03-18 22:37:22 +0100

    change homecontroller

--- test/src/main/java/org/example/controller/HomeController.java
+++ test/src/main/java/org/example/controller/HomeController.java
@@ -1,28 +1,38 @@
 package org.example.controller;
 
 import java.io.IOException;
-
 import java.sql.SQLException;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.example.service.EtudiantService;
 import org.example.service.EtudiantServiceImpl;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.servlet.ModelAndView;
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.example.model.Apprenant;
-import org.example.service.*;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping(value = "/")
 public class HomeController {
+	
 	
 	@Autowired
 	private EtudiantService etudiantService;
@@ -34,11 +44,17 @@ public class HomeController {
 	//	return new ModelAndView("test");
 //	}
 
-	@RequestMapping(value="/")
-	public ModelAndView test(HttpServletResponse response) throws IOException{
-		return new ModelAndView("home");
-	}
-
+//	@RequestMapping(value="/")
+//	public ModelAndView test(HttpServletResponse response) throws IOException{
+//		return new ModelAndView("home");
+//	}
+	@RequestMapping(value="/", method = RequestMethod.GET)
+    public String test(Model model) throws IOException{
+        model.addAttribute("user", user);
+       return "redirect:/home";
+    }
+	
+	
     @RequestMapping(value = "/home", method = RequestMethod.GET)
 	   public String home() {
 	      return "home";
@@ -66,34 +82,7 @@ public class HomeController {
 	      return "editReservation";
 	  }
   
-     
-//     @RequestMapping(value ="/login" ,method= RequestMethod.POST)
-// 	public ModelAndView register(@ModelAttribute("apprenant") Apprenant apprenant, BindingResult bindingResult){
-// 		if(bindingResult.hasErrors()){
-// 			return new ModelAndView("register");
-// 		}
-// 		getApprenantService().registerApprenant(apprenant);
-//
-// 		ModelAndView modelAndView = new ModelAndView("confirmation");
-// 		modelAndView.addObject("apprenant", apprenant);
-// 		return modelAndView;
-// 	}
-     
-     
-//     private void authenticate(HttpServletRequest request, HttpServletResponse response)
-// 			throws Exception {
-// 		String email = request.getParameter("email");
-// 		String password = request.getParameter("password");
-//
-// 		if (ApprenantService.validateApprenant(email, password) == true) {
-// 			login();
-// 		} else {
-// 			throw new Exception("Login not successful..");
-// 		}
-// 	}
-//	
-     
-     
+	
 //	@RequestMapping(value = "/index", method = RequestMethod.GET)
 //	public String index(ModelMap modelMap) throws ClassNotFoundException, SQLException {
 //		modelMap.put("etudiants", etudiantService.findAll());
