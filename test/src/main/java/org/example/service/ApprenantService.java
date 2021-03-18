595f90d0e931cb2a7207325934faf8c600050bae
lamyita <50775487+lamyita@users.noreply.github.com>
2021-03-18 22:37:59 +0100

    add user service

--- test/src/main/java/org/example/service/ApprenantService.java
+++ test/src/main/java/org/example/service/ApprenantService.java
@@ -1,11 +1,11 @@
-package org.example.service;
-
-import org.example.model.Apprenant;
-
-public interface ApprenantService {
-	
-	
-    public boolean  validateApprenant(String email, String password);
-    public boolean registerApprenant(Apprenant apprenant); 
-
-}
+//package org.example.service;
+//
+//import org.example.model.Apprenant;
+//
+//public interface ApprenantService {
+//	
+//	
+//    public boolean  validateApprenant(String email, String password);
+//    public boolean registerApprenant(Apprenant apprenant); 
+//
+//}
---
+++ test/src/main/java/org/example/service/UserService.java
@@ -0,0 +1,16 @@
+package org.example.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.example.model.Users;
+
+public interface UserService {
+
+	 public List<Users> findAll() throws ClassNotFoundException, SQLException;
+	    public Users findById(Long id) throws ClassNotFoundException, SQLException;
+	    public void save(Users user) throws ClassNotFoundException, SQLException;
+	    public void delete(Long id) throws ClassNotFoundException, SQLException;
+
+	    public boolean validate(String email,String password) throws ClassNotFoundException, SQLException;
+}
